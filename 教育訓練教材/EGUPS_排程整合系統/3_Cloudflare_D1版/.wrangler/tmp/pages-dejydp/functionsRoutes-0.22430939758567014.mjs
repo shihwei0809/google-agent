6b627e892f3b72cf52b2be05ead0b89e7a919602
shihwei0809 <shihwei0809@gmail.com>
2026-09-26 17:09:57 +0800

    feat: 完成排程接續排序、UI 微調及建立 Cloudflare D1 三軌架構

---
+++ 教育訓練教材/EGUPS_排程整合系統/3_Cloudflare_D1版/.wrangler/tmp/pages-dejydp/functionsRoutes-0.22430939758567014.mjs
@@ -0,0 +1,27 @@
+import { onRequestPost as __api_archive_js_onRequestPost } from "C:\\GOOGLE ANGET\\教育訓練教材\\EGUPS_排程整合系統\\2_PWA_App版\\functions\\api\\archive.js"
+import { onRequestGet as __api_data_js_onRequestGet } from "C:\\GOOGLE ANGET\\教育訓練教材\\EGUPS_排程整合系統\\2_PWA_App版\\functions\\api\\data.js"
+import { onRequestPost as __api_save_js_onRequestPost } from "C:\\GOOGLE ANGET\\教育訓練教材\\EGUPS_排程整合系統\\2_PWA_App版\\functions\\api\\save.js"
+
+export const routes = [
+    {
+      routePath: "/api/archive",
+      mountPath: "/api",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_archive_js_onRequestPost],
+    },
+  {
+      routePath: "/api/data",
+      mountPath: "/api",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_data_js_onRequestGet],
+    },
+  {
+      routePath: "/api/save",
+      mountPath: "/api",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_save_js_onRequestPost],
+    },
+  ]
